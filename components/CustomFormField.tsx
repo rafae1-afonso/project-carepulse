df0f22ac7c3481de56c3b4ff0fdd5f9b8e3d1d8f
Rafael Afonso <rafael.afonsof.lima@gmail.com>
2024-08-08 21:55:22 -0300

    register page 1

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -17,7 +17,9 @@ import Image from "next/image";
 import 'react-phone-number-input/style.css';
 import PhoneInput from 'react-phone-number-input';
 import { E164Number } from "libphonenumber-js/core";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 
 interface CustomProps {
     control: Control<any>,
@@ -35,7 +37,7 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
-    const { fieldType, iconSrc, iconAlt, placeholder } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder, dateFormat, showTimeSelect } = props;
 
     switch (props.fieldType) {
         case FormFieldType.INPUT:
@@ -73,6 +75,30 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                     onChange={field.onChange}
                     className="input-phone"
                 />
+            )
+
+        case FormFieldType.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image
+                        src='/assets/icons/calendar.svg'
+                        height={24}
+                        width={24}
+                        alt="calendar"
+                        className="ml-2"
+                    />
+
+                    <FormControl>
+                        <DatePicker 
+                            selected={field.value}
+                            onChange={(date) => field.onChange(date)}
+                            dateFormat={dateFormat ?? 'MM/dd/yyyy'}
+                            showTimeSelect={showTimeSelect ?? false}
+                            timeInputLabel="Time"
+                            wrapperClassName="date-picker"
+                        />
+                    </FormControl>
+                </div>
             )
 
         default:
